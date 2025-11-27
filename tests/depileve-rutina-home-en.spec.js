f6e772a4b4f8e1838b50fea55726aca7042a5206
yann1993 <101195445+yann1993@users.noreply.github.com>
2025-11-27 22:51:09 +0100

    Delete tests directory

--- tests/depileve-rutina-home-en.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://depileve.com/');
-  await page.getByRole('link', { name: 'Waxing Academy' }).click();
-  await page.getByRole('link', { name: 'Our Story' }).click();
-});
--- tests/depileve-rutina-inicio-es.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://depileve.com/es/');
-  await page.getByRole('link', { name: 'Waxing Academy' }).click();
-  await page.getByRole('link', { name: 'Nuestra historia' }).click();
-  await page.getByRole('link', { name: 'Contacto' }).click();
-});
